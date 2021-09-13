9cbbb604f363cd4411162e19d17bb6e3a98df615
Mit Bailey <mitbailey99@gmail.com>
2021-09-13 15:15:53 -0400

    Integrated spacehauc/positioner, now sends az el lat lon alt info across network.

--- include/track.hpp
+++ include/track.hpp
@@ -15,14 +15,15 @@
 #define TRACK_HPP
 
 #define SEC *1000000
-
 #define CMD_UPDATE_TLE 1
+#define DEFAULT_TLE1 "1 25544U 98067A   21229.77243765  .00001431  00000-0  34209-4 0  9998"
+#define DEFAULT_TLE2 "2 25544  51.6441  38.1681 0001381 320.9423  62.5381 15.48912726298140"
 
 typedef struct
 {
     NetDataClient *netdata;
-    char TLE1[64];
-    char TLE2[64];
+    char TLE1[100];
+    char TLE2[100];
 } global_data_t;
 
 typedef struct
@@ -31,6 +32,15 @@ typedef struct
     char TLE1[64];
     char TLE2[64];
 } track_cmd_t;
+
+typedef struct
+{
+    int az;
+    int el;
+    double lat;
+    double lon;
+    double alt;
+} track_data_t;
 
 void *gs_network_rx_thread(void *args);
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,26 @@
  * 
  */
 
-#include <stdio.h>
-#include <math.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
+#include <string.h>
+#include <TargetSystem.hpp>
+#include "clkgen.h"
+#include "meb_debug.h"
 #include <signal.h>
-#include "network.hpp"
-#include "meb_debug.hpp"
-#include <pthread.h>
-#include "track.hpp"
-
-#define lower_Az 40
-#define upper_Az 110
-#define lower_El 10
-#define upper_El 35
 
 volatile sig_atomic_t done = 0;
+
 void sighandler(int sig)
 {
     done = 1;
 }
 
-// Generates AzEl data for a fake satellite pass.
-
-int main()
+int main(int argc, char *argv[])
 {
     // Ignores broken pipe signal, which is sent to the calling process when writing to a nonexistent socket (
     // see: https://www.linuxquestions.org/questions/programming-9/how-to-detect-broken-pipe-in-c-linux-292898/
-    // and 
+    // and
     // https://github.com/sunipkmukherjee/example_imgui_server_client/blob/master/guimain.cpp
     // Allows manual handling of a broken pipe signal using 'if (errno == EPIPE) {...}'.
     // Broken pipe signal will crash the process, and it caused by sending data to a closed socket.
@@ -49,47 +39,39 @@ int main()
     global->netdata = new NetDataClient(NetPort::TRACK, SERVER_POLL_RATE);
     global->netdata->recv_active = true;
 
+    strcpy(global->TLE1, DEFAULT_TLE1);
+    strcpy(global->TLE2, DEFAULT_TLE2);
+
     // Create GSN thread IDs.
     pthread_t net_polling_tid, net_rx_tid;
 
-    signal(SIGINT, sighandler);
-    int AzEl[2] = {0};
-    bool upward = true;
-    bool iteratedLast = false;
+    // TODO: Send a network packet with the current AzEl data each time it is updated. Example below.
+    // { AZ-EL TRACKING DATA REPORT EXAMPLE
+    //     int AzEl[2] = {0};
+    //     AzEl[0] = azimuth;
+    //     AzEl[1] = elevation;
+    //     NetFrame *AzElFrame = new NetFrame((unsigned char *) AzEl, sizeof(AzEl), NetType::TRACKING_DATA, NetVertex::CLIENT);
+    //     AzElFrame->sendFrame(global->netdata);
+    //     delete AzElFrame;
+    // }
 
-    int fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-    if (fd < 3)
+    TargetSystem tsys;
+    if (argc > 2)
     {
-        printf("Error opening /dev/ttyUSB0, check wiring and sudo\n");
-        return 0;
+        dbprintlf(FATAL "Invalid number of command-line arguments given.");
+        return -1;
     }
-    struct termios options[1];
-    tcgetattr(fd, options);
-    options->c_cflag = B2400 | CS8 | CLOCAL | CREAD;
-    options->c_iflag = IGNPAR;
-    options->c_oflag = 0;
-    options->c_lflag = 0;
-    tcflush(fd, TCIFLUSH);
-    tcsetattr(fd, TCSANOW, options);
-
-    // FILE *fp = fopen("data.txt", "w");
-
-    // TODO: Currently uses the simulation (fake tracking) code. Update to using actual TLE data.
-
-    char cmd[50];
-    ssize_t sz;
-    sz = sprintf(cmd, "PB %d\r", lower_Az);
-    if (write(fd, cmd, sz) < sz)
+    else if (argc == 2)
     {
-        printf("Error writing azimuth command\n");
+        tsys.Create(argv[1], TLE1, TLE2, 42.65578686304611, -71.32546893568428, 1);
     }
-    sleep(1);
-    sz = sprintf(cmd, "PA %d\r", lower_El);
-    if (write(fd, cmd, sz) < sz)
-    {
-        printf("Error writing elevation command\n");
-    }
-    sleep(30); 
+    else
+        tsys.Create(TLE1, TLE2, 42.65578686304611, -71.32546893568428, 8);
+
+    clkgen_t clk = create_clk(1000000000, tsys.TimerHandler, &tsys);
+
+    printf("Running tracker, Ctrl+C to exit\n");
+
     while (!done)
     {
         // Not initialized or recoverable failure - thread boot required.
@@ -101,74 +83,30 @@ int main()
                 pthread_create(&net_polling_tid, NULL, gs_polling_thread, global->netdata);
                 pthread_create(&net_rx_tid, NULL, gs_network_rx_thread, global);
             }
-            else 
+            else
             {
                 dbprintlf(RED_FG "Cannot reboot network receive and poll threads, connection not ready.");
             }
         }
 
-        // TODO: Send a network packet with the current AzEl data each time it is updated. Example below.
-        // { AZ-EL TRACKING DATA REPORT EXAMPLE
-        //     int AzEl[2] = {0};
-        //     AzEl[0] = azimuth;
-        //     AzEl[1] = elevation;
-        //     NetFrame *AzElFrame = new NetFrame((unsigned char *) AzEl, sizeof(AzEl), NetType::TRACKING_DATA, NetVertex::CLIENT);
-        //     AzElFrame->sendFrame(global->netdata);
-        //     delete AzElFrame;
-        // }
-        
+        CoordTopocentric coord = tsys.GetPosition();
+        CoordGeodetic geo = tsys.GetGeoPosition();
+        printf("Target location: %d %d | %3.2lf %3.2lf %3.2lf\n", (int)coord.azimuth, (int)coord.elevation, geo.latitude, geo.longitude, geo.altitude);
 
-        for (int az = lower_Az; (az <= upper_Az) && (!done); az++)
-        {
-            double idx = (az - lower_Az) * M_PI / 70; // 0 -- pi
-            int el = (upper_El - lower_El) * sin(idx) + lower_El;
-            printf("Az: %d deg, El: %d deg\n", az, el);
-            
-            sz = snprintf(cmd, sizeof(cmd), "PB %d\r", az);
-            if (write(fd, cmd, sz) < sz)
-            {
-                printf("Error writing azimuth command\n");
-            }
-            sleep(1);
-            sz = snprintf(cmd, sizeof(cmd), "PA %d\r", el);
-            if (write(fd, cmd, sz) < sz)
-            {
-                printf("Error writing elevation command\n");
-            }
-            // if (!iteratedLast)
-            //     fprintf(fp, "%d %d\n", az, el);
-            sleep(1);
-        }
-        sz = sprintf(cmd, "PB %d\r", lower_Az);
-        if (write(fd, cmd, sz) < sz)
-        {
-            printf("Error writing azimuth command\n");
-        }
-        sz = sprintf(cmd, "PA %d\r", lower_El);
-        if (write(fd, cmd, sz) < sz)
-        {
-            printf("Error writing elevation command\n");
-        }
-        // if (iteratedLast == false)
-        // {
-        //     iteratedLast = true;
-        //     fclose(fp);
-        // }
-        for (int i = 0; (i < 60) && (!done); i++)
-            sleep(1);
+        // Send target location through the network.
+        track_data_t *info = {0};
+        info.az = (int)coord.azimuth;
+        info.el = (int)coord.elevation;
+        info.lat = geo.latitude;
+        info.lon = geo.longitude;
+        info.alt = geo.altitude;
+        NetFrame *dataFrame = new NetFrame((unsigned char *) info, sizeof(track_data_t), NetType::TRACKING_DATA, NetVertex::CLIENT);
+        dataFrame->sendFrame(global->netdata);
+        delete dataFrame;
+        sleep(1);
     }
-    sleep(1);
-    sz = sprintf(cmd, "PB %d\r", 0);
-    if (write(fd, cmd, sz) < sz)
-    {
-        printf("Error writing azimuth command\n");
-    }
-    sz = sprintf(cmd, "PA %d\r", 90);
-    sleep(1);
-    if (write(fd, cmd, sz) < sz)
-    {
-        printf("Error writing elevation command\n");
-    }
-    close(fd);
+
+    destroy_clk(clk);
+
     return 0;
 }
